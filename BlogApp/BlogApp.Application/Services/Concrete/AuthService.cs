cc5d47bcf365c74985bafd913951c76eccff7efb
Metin Seven <metinseven4748@gmail.com>
2026-06-30 14:32:20 +0300

    feat: implement password hashing and user mapping in AuthService Register

--- BlogApp/BlogApp.Application/Services/Concrete/AuthService.cs
+++ BlogApp/BlogApp.Application/Services/Concrete/AuthService.cs
@@ -1,9 +1,12 @@
 ﻿using BlogApp.Application.DTOs;
 using BlogApp.Application.Services.Abstract;
 using BlogApp.Core.Results;
+using BlogApp.Core.Security;
 using BlogApp.Data;
+using BlogApp.Data.Entities;
 using System;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace BlogApp.Application.Services.Concrete
@@ -12,10 +15,29 @@ namespace BlogApp.Application.Services.Concrete
     {
         public Result Register(RegisterRequest request)
         {
+            //business rule
             var userExists = context.Users.Any(u => u.Email == request.Email);
             if (userExists) 
                 return Result.Fail("This e-mail is already in use.");
-        
+
+            //hashing password
+            var hashResult = HashingHelper.CreatePasswordHash(request.Password);
+
+            //save to db
+            var user = new User
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                PasswordHash = hashResult.Hash,
+                PasswordSalt = hashResult.Salt,
+                IsActive = false
+            };
+
+
+
+
+
 
 
 
--- BlogApp/BlogApp.Core/Security/HashingHelper.cs
+++ BlogApp/BlogApp.Core/Security/HashingHelper.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Security.Cryptography;
 using System.Text;
 
 namespace BlogApp.Core.Security
 {
-    internal class HashingHelper
+    public static class HashingHelper
     {
+        public static HashResult CreatePasswordHash(string password)
+        {
+            using HMACSHA512 hmac = new();
+
+            byte[] passwordSalt = hmac.Key;
+            byte[] passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return new HashResult { Hash = passwordHash, Salt = passwordSalt };
+
+
+        }
+
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using HMACSHA512 hmac = new(passwordSalt);
+
+            byte[] computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            return computedHash.SequenceEqual(passwordHash);
+        }
     }
 }
+
+public class HashResult
+    {
+        public byte[] Hash { get; set; } = Array.Empty<byte>();
+        public byte[] Salt { get; set; } = Array.Empty<byte>();
+
+    }
+
