2e0e42b6dea3c71ad2ce699f289ec1787928569f
Metin Seven <metinseven4748@gmail.com>
2026-07-01 14:54:22 +0300

    feat: add RegisterViewModel and theme assests

---
+++ BlogApp/BlogApp.MVC/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using BlogApp.MVC.Models.ViewModels.AuthViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ BlogApp/BlogApp.MVC/Models/ViewModels/AuthViewModels/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.MVC.Models.ViewModels.AuthViewModels
+{
+    public class RegisterViewModel
+    {
+        //refactor into fluent validation library
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+        [Required, MaxLength(50)]
+        public string LastName { get; set; } = string.Empty;
+        [Required, MaxLength(100), EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required, MaxLength(100), DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
