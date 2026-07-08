84ef19482f16e72bab813b826acd68ad0fd85fc7
Metin Seven <metinseven4748@gmail.com>
2026-07-08 19:22:33 +0300

    feat: implement initial stage of BlogPostController

--- BlogApp/BlogApp.MVC/Controllers/BlogPostController.cs
+++ BlogApp/BlogApp.MVC/Controllers/BlogPostController.cs
@@ -1,23 +1,36 @@
 ﻿using BlogApp.Application.DTOs.Blog;
+using BlogApp.Application.Services.Abstract;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApp.MVC.Controllers
 {
-    public class BlogPostController : Controller
+    public class BlogPostController(IBlogPostService blogPostService) : Controller
     {
-        public IActionResult Content(int id)
+        public async Task<IActionResult> Content(int id)
         {
-            return View();
+            var result = await blogPostService.GetBlogPostById(id);
+
+            if (!result.Success || result.Data is null)
+                return RedirectToAction("Index", "Home");
+
+            var post = result.Data;
+
+            return View(post);
         }
 
+        [Authorize]
         public IActionResult Create()
         {
+            //TODO: Fill the model with categories and other necessary data for the view
             return View();
         }
 
         [HttpPost]
+        [Authorize]
         public IActionResult Create(CreateBlogPostRequest model)
         {
+            //TODO: Handle the creation of the blog post using the blogPostService.Get UserId from logged in user claims.
             return View();
         }
     }
